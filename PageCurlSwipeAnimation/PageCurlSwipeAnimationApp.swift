00ad82b8e16f60d2b0644b2028d55162427525ee
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-03-29 13:32:27 +0900

    Initial Commit

---
+++ PageCurlSwipeAnimation/PageCurlSwipeAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PageCurlSwipeAnimationApp.swift
+//  PageCurlSwipeAnimation
+//
+//  Created by 山本響 on 2023/03/29.
+//
+
+import SwiftUI
+
+@main
+struct PageCurlSwipeAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
